f9fded54c0a91d5de5fb9aaa8033b0b7aa71c62f
Arnold Noronha <arnstein87@gmail.com>
2013-08-20 22:50:18 -0400

    more stuff

--- lal-test-setup-test.el
+++ lal-test-setup-test.el
@@ -4,3 +4,8 @@
 (ert-deftest test-setup-basics ()
   (lal-delete-temp-project 
    (lal-create-temp-project)))
+
+(ert-deftest lal-with-temp-project-test ()
+  (with-temp-project
+   (should t)
+   (should t)))
--- lal-test-setup.el
+++ lal-test-setup.el
@@ -3,12 +3,27 @@
 
 (provide 'lal-test-setup)
 
+(defun validate-dir (dir)
+  (should (starts-with dir "/tmp"))) ;; verify we're not deleting something major
+
 (defun lal-create-temp-project ()
   (make-temp-file "emacsproject" t))
 
 (defun lal-delete-temp-project (dir)
-  (should (starts-with dir "/tmp")) ;; verify we're not deleting something major
+  (validate-dir dir)
   (delete-directory dir t))
+
+(defmacro with-temp-project (&rest body)
+  `(let ((tmp (lal-create-temp-project)))
+     (unwind-protect (progn ,@body)
+       (lal-delete-temp-project tmp))))
+  
+
+;; touch a filename
+(defun lal-touch (dir file)
+  (validate-dir)
+  (let ((abs (concat dir "/" file)))
+    (write-region "" nil abs)))
 
 
     
